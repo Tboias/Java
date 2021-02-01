d2574d5dc85f0785a9490a764294246111229a3a
Tboias <candidobr12@gmail.com>
2021-02-01 20:43:48 -0300

    Add 02

--- TesteCondicional.java
+++ TesteCondicional.java
@@ -1,12 +1,23 @@
 public class TesteCondicional {
     public static void main(String[] args) {
         System.out.println("Testando Condicionais");
-        int idade = 19;
-        if (idade >= 20) {
-            System.out.println("Você tem mais de 18 anos");
-            System.out.println("Seja Bem Vindo ! ");
+        int idade = 17;
+        int quantidadePessoas = 3;
+        if (idade >= 18) {
+
+            System.out.println(" Você é de maior.");
+            System.out.println(" Seja Bem Vindo ! ");
+
         } else {
-            System.out.println("Você tem menos de 18 anos");
+            if (quantidadePessoas >= 2) {
+
+                System.out.println(" Você é de menor.");
+                System.out.println(" Porem está acompanhado. ");
+                System.out.println(" Seja Bem Vindo ! ");
+
+            } else {
+                System.out.println(" Infelizmente voce nao pode entrar ");
+            }
         }
     }
 }
