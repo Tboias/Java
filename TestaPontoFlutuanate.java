16cf3508aa90c89d4cb29dba3d9c71c4b96d861d
Tboias <candidobr12@gmail.com>
2021-02-01 17:22:15 -0300

    Adicionando alguns arquivos

---
+++ TestaPontoFlutuanate.java
@@ -0,0 +1,7 @@
+public class TestaPontoFlutuanate {
+    public static void main(String[] args) {
+        double salario;
+        salario = 1250.50;
+        System.out.println("Meu salario é " + salario);
+    }
+}
--- TestaVariaveis.java
+++ TestaVariaveis.java
@@ -1,10 +1,17 @@
 public class TestaVariaveis {
     public static void main(String[] args) {
-        System.out.println("Essa é minha idade : ");
 
         int idade;
         idade = 16;
 
-        System.out.println(idade);
+        System.out.println("Essa é minha idade : " + idade);
+
+        idade = 4 * 4;
+
+        System.out.println("Essa é minha idade : " + idade);
+
+        idade = 18 - 2;
+
+        System.out.println("Essa é minha idade : " + idade);
     }
 }
