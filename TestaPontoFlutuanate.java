308a36c4a077197a8074bde056019dad2af1529c
Tboias <candidobr12@gmail.com>
2021-02-01 17:47:31 -0300

    Outros Adicionamentos

--- TestaPontoFlutuanate.java
+++ TestaPontoFlutuanate.java
@@ -1,7 +1,18 @@
 public class TestaPontoFlutuanate {
     public static void main(String[] args) {
-        double salario;
-        salario = 1250.50;
+        double salario = 1250.50;
         System.out.println("Meu salario é " + salario);
+
+        double divisao = 3.14 / 2;
+        System.out.println(divisao);
+
+        int outraDivisao = 5 / 2;
+        System.out.println(outraDivisao);
+
+        double novaTentativa = 5 / 2;
+        System.out.println(novaTentativa);
+
+        double novaTentativaDaTentavina = 5.0 / 2.0;
+        System.out.println(novaTentativaDaTentavina);
     }
 }
