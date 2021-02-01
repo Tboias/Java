0e59e31f9065492030b697994d6b7c13110ba51e
Tboias <candidobr12@gmail.com>
2021-02-01 14:57:14 -0300

    Adicionando Coisas

---
+++ TestaVariaveis.java
@@ -0,0 +1,10 @@
+public class TestaVariaveis {
+    public static void main(String[] args) {
+        System.out.println("Essa é minha idade : ");
+
+        int idade;
+        idade = 16;
+
+        System.out.println(idade);
+    }
+}
