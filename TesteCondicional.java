f83b5dcd4f289e39a2d57368a5b3b952a5d71c16
Tboias <candidobr12@gmail.com>
2021-02-01 21:36:16 -0300

    Add 03

--- TesteCondicional.java
+++ TesteCondicional.java
@@ -1,10 +1,13 @@
-
 public class TesteCondicional {
     public static void main(String[] args) {
-        System.out.println("Testando Condicionais");
-        int idade = 17;
+
+        // Variaveis
         int quantidadePessoas = 3;
-        if (idade >= 18 && quantidadePessoas >= 2) {
+        int idade = 20;
+        boolean acompanhado = quantidadePessoas >= 2;
+        // Variaveis
+
+        if (idade >= 18 && acompanhado) {
 
             System.out.println(" Seja Bem Vindo ! ");
 
